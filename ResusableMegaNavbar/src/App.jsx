4417ad9a4b2827b9671bd9b9e12f2df0b2cf1688
Luciano Cipolla Delcantare <cipollaluciano4@gmail.com>
2026-03-12 19:39:21 -0300

    ahora subiendo un footer, le estoy agarrando la mano a la estructura de archivos que se usa

--- ResusableMegaNavbar/src/App.jsx
+++ ResusableMegaNavbar/src/App.jsx
@@ -1,9 +1,10 @@
 import { Navbar } from "./Navbar"
-
+import { Footer } from "./Footer"
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
---
+++ ResusableMegaNavbar/src/Footer.jsx
@@ -0,0 +1,18 @@
+export function Footer(){
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">un link</a></li>
+        <li><a href="#">un link</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">un link</a></li>
+        <li><p> copyright © {new Date().getFullYear()}</p></li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </footer>
+  )
+}
